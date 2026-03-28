f1ed1c66e7a3a081ea1f83a3a513031b4204b985
Simran <kaursimran@gmail.com>
2026-03-28 18:31:59 +0530

    updated about section UI

--- frontend/src/Components/About/about.jsx
+++ frontend/src/Components/About/about.jsx
@@ -13,10 +13,10 @@ const About = () => {
             <div className="dot"></div>
             <div className="text">
               <h2>Personal Info</h2>
-              <p><span>NAME :</span> Ayush Sahu</p>
-              <p><span>AGE :</span> 21 Years</p>
-              <p><span>GENDER :</span> Male</p>
-              <p><span>LANGUAGE KNOWN :</span> Hindi, English</p>
+              <p><span>NAME :</span> Simranjeet Kaur</p>
+              <p><span>AGE :</span> 19 Years</p>
+              <p><span>GENDER :</span> Female</p>
+              <p><span>LANGUAGE KNOWN :</span> Punjabi, English</p>
             </div>
           </div>
 
@@ -25,8 +25,8 @@ const About = () => {
             <div className="text">
               <h2>Education</h2>
               <p><span>DEGREE :</span> BTECH</p>
-              <p><span>BRANCH :</span> Computer Science & Engineering</p>
-              <p><span>CGPA :</span> 8.2</p>
+              <p><span>BRANCH :</span> Information Technology </p>
+              <p><span>CGPA :</span> 7.31</p>
             </div>
           </div>
 
@@ -35,7 +35,7 @@ const About = () => {
             <div className="text">
               <h2>Skills</h2>
               <p>MERN Stack Web Developer</p>
-              <p>Java</p>
+              <p>C++</p>
               <p>DSA</p>
             </div>
           </div>
